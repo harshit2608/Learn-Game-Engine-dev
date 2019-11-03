e886e76e9de9efe15ba43293bec20ef376e2b023
harshit <harshitrock111@gmail.com>
2019-11-03 15:15:51 +0530

    vector

---
+++ Math engine/Vectors/ConsoleApplication1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "R4DVector.h"
+
+using namespace R4DEngine;
+
+int main(int argc, const char*argv[])
+{
+	R4DEngine::R4DVector3n vector1(2, 3, 1);
+	R4DEngine::R4DVector3n vector2(1, 2, 0);
+
+	return 0;
+}
