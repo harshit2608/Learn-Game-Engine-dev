e2d25001c657c2c4001baf0f7a98bc8422e195b9
harshit <harshitrock111@gmail.com>
2019-12-29 00:17:03 +0530

    SPDLOG INTRODUCED

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized HAZEL !");
+	HZ_CLIENT_INFO("Welcome to the Hazel");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
